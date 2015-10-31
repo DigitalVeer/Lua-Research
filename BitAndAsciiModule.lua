989f813f6f0f07a7f5c2fccdb8c5f105b9ede183
Veer Singh <digitalizedveer@gmail.com>
2015-10-31 12:13:48 -0400

    Create BitAndAsciiModule.lua

---
+++ BitAndAsciiModule.lua
@@ -0,0 +1,24 @@
+local misc={}
+function misc.getAlphabet(c,s,e)
+	local str = "";
+	for i = (c and"A"or"a"):byte(),(c and"Z"or"z"):byte() do
+		str=str..str.char(i);
+	end
+	return str:sub(s or 1, e or#str)
+end
+
+function misc.invBitString(s)
+	return (tonumber(_VERSION:match("5%.(%d+)"))>=3 and({(s:gsub(".",function(a)return(a+1)%2 end)):gsub("%..","")})[1] or ({s:gsub("1","2"):gsub("0","1"):gsub("2","0")})[1])
+end
+
+function misc.bitToDec(n)
+	local bit,dec=tostring(n),0;
+	for i=#bit,1,-1 do dec=dec+tonumber(bit:sub(i,i))* 2^(#bit-i)end
+	--tonumber(n,2)) is also a palpable solution
+	return dec;
+end
+
+function misc.decToBin(n)
+end
+
+return misc
